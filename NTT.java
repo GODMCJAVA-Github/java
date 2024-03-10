f8b9e38e27ccaabcacff0f6820f9dbc5f8c98a78
GODMCJAVA-Github <98897556+GODMCJAVA-Github@users.noreply.github.com>
2024-03-10 13:00:47 +0530

    Add files via upload

---
+++ NTT.java
@@ -0,0 +1,12 @@
+class test {
+
+    public static void main(String a[]) {
+
+        float nom = 10.8f;
+        float noe = 7.203049f + nom - 0.000000190734863f;
+        double nol = noe - 0.00305f;
+        System.out.print("the answer is = ");
+        System.out.print(nol);
+    }
+
+}
---
+++ vari.java
@@ -0,0 +1,13 @@
+class vari {
+    public static void main(String a[]) {
+
+        byte by = 126; // is only 1 byte rage is -127 to 126 but is fast af
+        float fl = 30.2f; // can have point and is 4 byte also have to always add an "f" like 40.05f
+        char ch = 'k'; // can have letters like a b or b even numbers like 9 3 5 but can using only one
+                       // letter & have to always have '' e.g 'o'
+        double duo = 394.294; // is 8 byte and can have numbers w/ point e.g 203.55 without F or using flort
+        boolean boo = true; // or false
+        int int1 = 102; // 10 outa 10 will do many times 4 byte ez to use fun and fast :)
+        System.out.println(by + fl + ch + duo + int1);
+    }
+}
